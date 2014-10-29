24bdd2f6af795208be955d514950a652102a5121
simplejoy <lantian-lz@163.com>
2014-10-29 22:26:47 +0800

    url完善

--- www/weixin/urls.py
+++ www/weixin/urls.py
@@ -3,5 +3,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('www.weixin.views',
-                       url(r'^$', 'index'),
+                       url(r'^/?$', 'index'),
                        )
