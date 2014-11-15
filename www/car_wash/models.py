a96a01e363d518a488927f28ce0394e42469424d
simplejoy <lantian-lz@163.com>
2014-11-15 18:10:17 +0800

    优惠券建模完成

--- www/car_wash/models.py
+++ www/car_wash/models.py
@@ -122,7 +122,49 @@ class ServicePrice(models.Model):
         ordering = ["sort_num", "sale_price"]
         unique_together = [("car_wash", "service_type"), ]
 
+
+class Coupon(models.Model):
+
+    """
+    @note: 优惠券
+    """
+    coupon_type_choices = ((0, u'优惠特定金额'), (1, u'优惠至特定金额'))
+    state_choices = ((0, u'未领取'), (1, u'未使用'), (2, u'已使用'))
+
+    code = models.CharField(max_length=64, unique=True)  # 优惠券编码
+    coupon_type = models.IntegerField(default=1, choices=coupon_type_choices)
+    user_id = models.CharField(max_length=36, db_index=True, null=True)  # 用户
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # 优惠幅度，小于1代表折扣率，大于1代表折扣金额
+    expiry_time = models.DateTimeField()  # 失效时间
+    minimum_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # 最低消费额
+    car_wash = models.ForeignKey("CarWash")
+
+    create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
+    state = models.IntegerField(default=1, db_index=True, choices=state_choices)
+    email_flag = models.BooleanField(default=False)  # 标记是否发送过优惠卷过期提醒消息
+    data_body = models.TextField(default='')
+
+    class Meta:
+        ordering = ['-id', ]
+
+    def __unicode__(self):
+        return str(self.code)
+
+    def is_expiry(self):
+        return self.expiry_time < datetime.datetime.now()
+
+    def get_user_note(self):
+        '''
+        @note: 获取使用说明
+        '''
+        note = u'全场通用，无限制条件'
+        if self.coupon_type == 0:
+            note = u'最低消费%s元' % self.minimum_amount
+        else:
+            car_wash = self.car_wash
+            note = u'仅限洗车行 <a href="%s">%s</a>使用' % (car_wash.get_url(), car_wash.name)
+        return note
+
 """
 订单
-优惠码
 """
