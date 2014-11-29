f998667840e401d12997e92e474cf63883c18a91
simplejoy <lantian-lz@163.com>
2014-11-29 12:36:39 +0800

    1.admin后台完善 2.最后活跃时间通过中间件控制 3.默认图像添加 4.文件版本号通过缓存获取

--- www/middleware/user_middleware.py
+++ www/middleware/user_middleware.py
@@ -19,6 +19,8 @@ class UserMiddware(object):
         request.sub_domain = sub_domain
 
     def process_response(self, request, response):
+        from www.account.interface import UserBase
+
         if hasattr(request, '_process_start_timestamp'):
             t = int((time.time() - float(getattr(request, '_process_start_timestamp'))))
             if t >= 10:
@@ -26,6 +28,11 @@ class UserMiddware(object):
                 logging.error("LONG_PROCESS: %s %s %s" % (request.path, t, user_id))
 
         # 更新活跃时间
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            user_agent = request.META.get("HTTP_USER_AGENT", "").lower()
+            last_active_source = 2 if "micromessenger" in user_agent else 0
+            UserBase().update_user_last_active_time(request.user.id, ip=utils.get_clientip(request), last_active_source=last_active_source)
+
         return response
 
     def process_exception(self, request, exception):
