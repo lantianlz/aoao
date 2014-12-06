19f62b05f7b20ee7fc78af5f5698533911b64044
lei286394973 <286394973@qq.com>
2014-12-06 15:24:52 +0800

    二维码扫描登录

---
+++ www/shop/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
+
+urlpatterns = patterns('www.shop.views', 
+					url(r'^verify_code$', 'verify_code'),
+					)
--- www/shop/views.py
+++ www/shop/views.py
@@ -13,3 +13,7 @@ from www.misc.decorators import member_required
 
 def login_shop(request, template_name='pc/shop/login_shop.html'):
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+
+
+def verify_code(request, template_name='pc/shop/verify_code.html'):
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
